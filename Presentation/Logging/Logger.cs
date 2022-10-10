522bb2a3a7ecdd84de48a92f635ff4d9ea695bc3
Scover <thediscover22450@gmail.com>
2022-10-10 17:59:25 +0200

    Fixed SA1402

--- Presentation/Logging/Logger.cs
+++ Presentation/Logging/Logger.cs
@@ -42,14 +42,3 @@ public abstract class Logger
                             int CallLine,
                             string CallFile);
 }
-
-public static class LoggerExtensions
-{
-    /// <inheritdoc cref="Logger.Log(string, LogLevel, string, int, string)"/>
-    public static void Log(this string message,
-             LogLevel lvl = LogLevel.Verbose,
-             [CallerMemberName] string caller = "",
-             [CallerLineNumber] int callLine = 0,
-             [CallerFilePath] string callFile = "")
-        => App.Logger.Log(message, lvl, caller, callLine, callFile);
-}
